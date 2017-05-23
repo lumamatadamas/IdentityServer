89bc9f0f50a0bac435199eab9c043665ebf3b6bb
Martin Matadamas <luismartin@ssf.com.mx>
2017-05-23 11:17:27 -0500

    add client and config needed to run async main method

---
+++ Client/Program.cs
@@ -0,0 +1,77 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        /*static void Main(string[] args)
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+                Console.WriteLine(response.StatusCode);
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }*/
+
+        static void Main(string[] args)
+        {
+            Task.Run(() => MainAsync(args)).GetAwaiter().GetResult();
+
+            //Task.Run(async () => await MainAsync(new string[] { "a", "b" }));
+            //AsyncContext.Run(() => MainAsync(args));
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+                Console.WriteLine(response.StatusCode);
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
