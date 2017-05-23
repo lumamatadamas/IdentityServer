cd8f9a88d890846e6c8f065275a7faa9aaaebc3b
Martin Matadamas <luismartin@ssf.com.mx>
2017-05-23 12:48:30 -0500

    delete comments

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,34 +8,6 @@ namespace Client
 {
     class Program
     {
-        /*static void Main(string[] args)
-        {
-            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
-
-            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
-            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
-
-            if (tokenResponse.IsError)
-            {
-                Console.WriteLine(tokenResponse.Error);
-                return;
-            }
-
-            Console.WriteLine(tokenResponse.Json);
-
-            var client = new HttpClient();
-            client.SetBearerToken(tokenResponse.AccessToken);
-
-            var response = await client.GetAsync("http://localhost:5001/identity");
-            if (!response.IsSuccessStatusCode)
-                Console.WriteLine(response.StatusCode);
-            else
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(JArray.Parse(content));
-            }
-        }*/
-
         static void Main(string[] args)
         {
             Task.Run(() => MainAsync(args)).GetAwaiter().GetResult();
