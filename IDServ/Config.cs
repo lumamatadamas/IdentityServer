db40173285a16b790f7c2e242a4dcaa0cce19ab9
Martin Matadamas <luismartin@ssf.com.mx>
2017-05-22 21:17:08 -0500

    Agregar archivos de proyecto.

---
+++ IDServ/Config.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IDServ
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    //no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    //secrete for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
